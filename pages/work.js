e2b1237aeb9e6ccd716e598a696a6569012ab46a
TheNoah13XD <broken.personal.1211@gmail.com>
2023-11-24 22:00:02 +0530

    del: notion api

--- pages/work.js
+++ pages/work.js
@@ -172,27 +172,3 @@ export default function Work({ results }) {
         </>
     );
 }
-
-export async function getStaticProps() {
-    const notion = new Client({ auth: process.env.NOTION_API_KEY });
-    const response = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID,
-        filter: {
-            property: 'Status',
-            status: {
-                equals: 'Completed'
-            }
-        },
-        sorts: [
-            {
-                property: 'S.No',
-                direction: 'ascending',
-            },
-        ]
-    });
-    return {
-        props: {
-            results: response.results,
-        }
-    }
-}
